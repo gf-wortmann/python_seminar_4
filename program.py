b5a80258aabe3fb207fd3aa55c35b590b8f78b0e
George Wortmann <gf.wortmann@gmail.com>
2023-04-02 20:22:13 +0300

    task 27 committed 1

--- program.py
+++ program.py
@@ -1,21 +1,18 @@
-# Задача №25. Решение в группах
-# Напишите программу, которая принимает на вход
-# строку, и отслеживает, сколько раз каждый символ
-# уже встречался. Количество повторов добавляется к
-# символам с помощью постфикса формата _n.
-# Input: a a a b c a a d c d d
-# Output: a a_1 a_2 b c a_3 a_4 d c_1 d_1 d_2
+# Задача №27. Решение в группах
+# Пользователь вводит текст(строка). Словом считается
+# последовательность непробельных символов идущих
+# подряд, слова разделены одним или большим числом
+# пробелов. Определите, сколько различных слов
+# содержится в этом тексте.
+# Input: She sells sea shells on the sea shore
+# #The shells, that she sells are sea shells I'm sure.
+# So if she sells sea shells on the sea shore
+# I'm sure that the shells are sea shore shells
+# Output: 13
 
-stringEntered = 'a a a b c a a d c d d'
-stringSplit = stringEntered.split(' ')
-stringResult = ''
-dictLetters = {i:0 for i in stringSplit}
-
-for j in range(len(stringSplit)):
-    stringResult += f' {stringSplit[j]}'
-    if dictLetters[stringSplit[j]] != 0:
-        stringResult += f'_{dictLetters[stringSplit[j]]} '
-    dictLetters[stringSplit[j]] += 1
-
-stringResult = stringResult[1:-1]
-print(stringResult)
+string_Text = "She sells sea shells on the sea shore" \
+              "The shells that she sells are sea shells. I'm sure." \
+              "So if she sells sea shells on the sea shore" \
+              "I'm sure that the shells are sea shore shells"
+set_count = set(string_Text.lower().split(' '))
+print(len(set_count))
